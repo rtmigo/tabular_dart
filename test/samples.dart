829a8236ebc86abe0e325b2c39843731e4c4384e
rtmigo <art.galkin@gmail.com>
2021-03-20 05:10:12 +0300

    docs

--- test/samples.dart
+++ test/samples.dart
@@ -2,12 +2,12 @@ import 'package:tabular/src/inner.dart';
 
 void main() {
   print(tabular([
-    ['Country', 'Islands'],
-    ['Sweden', 221800],
-    ['Finland', 188000],
-    ['Norway', 55000],
-    ['Canada', 52455],
-    ['Indonesia', 17508],
-    ['Japan', 6853],
+    ['Country', 'Islands', 'Population'],
+    ['Sweden', 221800, 10099265],
+    ['Finland', 188000, 5540720],
+    ['Norway', 55000, 5421241],
+    ['Canada', 52455, 37742154],
+    ['Indonesia', 17508, 273523615],
+    ['Japan', 6853, 126476461],
   ]));
 }
