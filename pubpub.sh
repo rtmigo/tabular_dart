840fa8b5b7834337912173f46a7ee589a5515c87
rtmigo <art.galkin@gmail.com>
2021-03-21 23:28:31 +0300

    minor

--- pubpub.sh
+++ pubpub.sh
@@ -18,28 +18,18 @@ rsync -Rrv ./ "$temp_pub_dir" \
   --exclude=".github" \
   --exclude="labuda/" \
   --exclude="draft/" \
+  --exclude="analysis_options.yaml" \
   --exclude="experiments"
-
-#insert=":warning: **At the time of writing, pub.dev does not display \
-#the program output correctly. See the \
-#[Tabular readme on GitHub](https://github.com/rtmigo/tabular#readme).**"
 
 # replacing \n by \r
 old_readme=$(cat README.md | tr '\n' '\r')
 # removing everything before "\n# ", the first header
 new_readme=$(echo "$old_readme" | perl -p0e 's|^.*?\r# |# \1|')
-# inserting the pub.dev formatting warning
-#new_readme=$(echo "$new_readme" | perl -p0e "s|\`\`\` text|$insert\\r\\r\`\`\` text \1|")
-# replacing \r back to \n
+# replacing \r by \n
 new_readme=$(echo "$new_readme" |  tr '\r' '\n')
 
 echo "$new_readme" > "$temp_pub_dir/README.md"
 
 cd "$temp_pub_dir"
 dartfmt -w .
-#dart pub publish --dry-run
 dart pub publish
-#open "$temp_pub_dir"
-
-# :warning: **At the time of writing, pub.dev does not display the program output
-# correctly. See the [Tabular readme on GitHub](https://github.com/rtmigo/tabular#readme).**
