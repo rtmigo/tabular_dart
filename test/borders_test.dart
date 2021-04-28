e4b1bbeb8feb697cf67055e97e3b214a7dbd125e
rtmigo <art.galkin@gmail.com>
2021-04-28 21:19:57 +0300

    dividers

--- test/borders_test.dart
+++ test/borders_test.dart
@@ -8,26 +8,6 @@ import 'common.dart';
 
 void main() {
 
-  var splitline_date = [
-    ['Season', '#', 'Name', 'Days', 'Sun'],
-    ['Winter', 12, 'December', 31, 258],
-    ['Winter', 1, 'January', 31, 94],
-    ['Winter', 2, 'February', 28, 123],
-    split_line,
-    ['Spring', 3, 'March', 31, 42],
-    ['Spring', 4, 'April', 30, 243],
-    ['Spring', 5, 'May', 31, 5523],
-    split_line,
-    ['Summer', 6, 'June', 30, 11251],
-    ['Summer', 7, 'July', 31, 17451],
-    ['Summer', 8, 'August', 31, 18707],
-    split_line,
-    ['Autumn', 9, 'September', 30, 7025],
-    ['Autumn', 10, 'October', 31, 5041],
-    ['Autumn', 11, 'November', 30, 2302],
-
-  ];
-
   group('markdown', () {
 
     test('no border', () {
@@ -157,19 +137,5 @@ void main() {
         +----------+--------+--------+-----------+
         '''.trim().noSpaces());
     });
-
-    // test('all and split', () {
-    //   var t = tabular(months, style: Style.mysql, border: Border.vertical);
-    //   //print(t.spaceToDot());
-    //
-    //   expect(t.spaceToDot(), '''
-    //     |.Winter...|.Spring.|.Summer.|.Autumn....|
-    //     +----------+--------+--------+-----------+
-    //     |.December.|.March..|.June...|.September.|
-    //     |.January..|.April..|.July...|.October...|
-    //     |.February.|.May....|.August.|.November..|
-    //     '''.trim().noSpaces());
-    // });
-
   });
 }
