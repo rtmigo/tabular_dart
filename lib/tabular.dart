a5530ab84bf9f75173b437fcedff3c4deaf30ba5
rtmigo <art.galkin@gmail.com>
2021-04-28 21:00:19 +0300

    publish

--- lib/tabular.dart
+++ lib/tabular.dart
@@ -1,5 +1,4 @@
 // SPDX-FileCopyrightText: (c) 2021 Artёm IG <github.com/rtmigo>
 // SPDX-License-Identifier: MIT
 
-
 export 'src/inner.dart' show tabular, Sort, Side, FormatCell, CompareCells, split_line, Style, Border;
